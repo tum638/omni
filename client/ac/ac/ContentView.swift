b5ba22f65af34b3041b89e68724f825054f2403e
Giovanni Maya <giovannimhern@uchicago.edu>
2025-06-26 11:54:40 -0700

    config: initialize swift app

---
+++ client/ac/ac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ac
+//
+//  Created by Giovanni Maya on 6/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ client/ac/ac/acApp.swift
@@ -0,0 +1,17 @@
+//
+//  acApp.swift
+//  ac
+//
+//  Created by Giovanni Maya on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct acApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
